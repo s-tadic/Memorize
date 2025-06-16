b539ec25cc19dc00f6ead11d94fab788b9e3f5f6
Stefan Tadic <stefan.tadic@me.com>
2025-06-16 08:23:13 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stefan Tadić on 16. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
